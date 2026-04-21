d26e0db4fc77fddab3fd6899e647f1aeacc4a393
wasay <wasay56411@gmail.com>
2026-04-22 00:19:04 +0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,16 +23,23 @@ const footerText = [
 const Footer = () => {
   return (
     <div className="max-w-350 mx-auto bg-[#12253E] h-auto pt-10 pb-5 md:pb-0">
-      {/* Top Section */}
       <div className="lg:flex justify-between gap-4 px-5">
         <div>
-          <img className="w-full h-auto lg:w-140 lg:h-70 object-cover" src="/Footer.png" alt="" />
+          <img
+            className="w-full h-auto lg:w-140 lg:h-70 object-cover"
+            src="/Footer.png"
+            alt=""
+          />
           <div className="flex justify-between py-3">
-            <p className="font-montserrat text-sm sm:text-xl text-[#FFFFFF]">Stay Connected</p>
-            <p className="font-montserrat text-sm sm:text-xl text-[#FFFFFF]">contact@minhatech.org</p>
+            <p className="font-montserrat text-sm sm:text-xl text-[#FFFFFF]">
+              Stay Connected
+            </p>
+            <p className="font-montserrat text-sm sm:text-xl text-[#FFFFFF]">
+              contact@minhatech.org
+            </p>
           </div>
         </div>
-        
+
         {footerText.map((item) => (
           <div className="py-5 lg:py-0" key={item.id}>
             <h1 className="text-[#FFFFFF] font-semibold pb-2 font-montserrat text-2xl">
@@ -40,7 +47,10 @@ const Footer = () => {
             </h1>
             {[item.text, item.text1, item.text2, item.text3].map(
               (text, idx) => (
-                <p className="text-base text-[#FFFFFF] font-montserrat py-2" key={idx}>
+                <p
+                  className="text-base text-[#FFFFFF] font-montserrat py-2"
+                  key={idx}
+                >
                   {text}
                 </p>
               )
@@ -60,21 +70,25 @@ const Footer = () => {
           </div>
         </div>
       </div>
-
-      {/* Bottom Section - No Absolute, Just Static Flow */}
       <div className="pt-5 pb-2 px-5">
-        <hr className="text-[#D9D9D96B]"/>
+        <hr className="text-[#D9D9D96B]" />
         <div className="flex flex-wrap justify-between gap-4 py-5">
-            <p className="hidden md:block font-montserrat font-medium text-sm text-[#FFFFFFC7]">©2025-2026 Minha Tech. All Rights Reserved</p>
-            <p className="font-montserrat font-medium text-sm text-[#FFFFFFC7]">Terms of Use</p>
-            <p className="font-montserrat font-medium text-sm text-[#FFFFFFC7]">Privacy Policy</p>
+          <p className="hidden md:block font-montserrat font-medium text-sm text-[#FFFFFFC7]">
+            ©2025-2026 Minha Tech. All Rights Reserved
+          </p>
+          <p className="font-montserrat font-medium text-sm text-[#FFFFFFC7]">
+            Terms of Use
+          </p>
+          <p className="font-montserrat font-medium text-sm text-[#FFFFFFC7]">
+            Privacy Policy
+          </p>
         </div>
       </div>
       <div className="px-5 pb-5">
-        <img  src="/MinhaTechText.png" alt="" />
+        <img src="/MinhaTechText.png" alt="" />
         <p className="block md:hidden font-montserrat font-medium text-sm text-[#FFFFFFC7] mt-3">
           ©2025-2026 Minha Tech. All Rights Reserved
-        </p> 
+        </p>
       </div>
     </div>
   );
