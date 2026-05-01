c130284aa66632046085b4a2ef3cc87f5bc36a74
wasay <wasay56411@gmail.com>
2026-05-02 00:39:35 +0500

    update

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Heading = ({title, para, children}) => {
   return (
     <div>
-      <div className='flex justify-center items-center text-center py-14'>
+      <div className='flex justify-center items-center text-center py-10'>
         <div>
-        <h1 className='text-2xl sm:text-5xl md:text-6xl text-[#FFFFFF] font-semibold font-montserrat '>{title}</h1>
-        <p className='text-[#C6C7C8] text-base sm:text-lg md:text-xl text-center pt-3'>{para}</p>
+        <h1 className='text-2xl sm:text-5xl  text-[#FFFFFF] font-semibold font-montserrat '>{title}</h1>
+        <p className='text-[#C6C7C8] text-sm sm:text-lg  text-center pt-4'>{para}</p>
         </div>
       </div>
     </div>
