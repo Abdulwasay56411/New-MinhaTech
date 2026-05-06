5bcf9ed3eba9a128d05707fc49dae3a876ba8d5c
wasay <wasay56411@gmail.com>
2026-05-06 23:48:23 +0500

    update

--- src/components/Home/OurProject.jsx
+++ src/components/Home/OurProject.jsx
@@ -105,7 +105,7 @@ const OurProject = () => {
         {project.map((item) => (
           <div key={item.id} className='bg-[#3E496166] w-full h-auto py-5 px-8 rounded-3xl shadow-[0_0_61px_rgba(6,184,255,0.14)]'>
             <img className='w-full h-auto object-cover' src={item.image} alt="" />
-            <h1 className='font-bold text-lg sm:text-3xl text-[#F9F9F9] pt-8 md:py-8'>{item.title}</h1>
+            <h1 className='font-bold text-lg sm:text-2xl text-[#F9F9F9] pt-8 md:py-8'>{item.title}</h1>
             {item.sections.map((sec, i) => (
               <div key={i} className='py-2'>
                 <h1 className='font-bold text-base sm:text-xl text-[#06A9EA] py-1'>{sec.label}</h1>
